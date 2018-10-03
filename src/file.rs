c8426283fafc71c99a9b72112f0b28969b437b52
Aloxaf <bailong104@gmail.com>
2018-10-03 22:47:12 +0800

    Add decipher

--- src/file.rs
+++ src/file.rs
@@ -3,32 +3,34 @@ use std::fs::File;
 use std::io::prelude::*;
 use zip::ZipArchive;
 
-
-pub fn load_stream(is: &File, size: usize) -> Vec<u8> {
-    is.take(size as u64)
+pub fn load_file(filename: &str, size: usize) -> Result<Vec<u8>, Error> {
+    let bytes = open_input(filename)?
+        .take(size as u64)
         .bytes()
         .map(|c| c.unwrap())
-        .collect::<Vec<_>>()
+        .collect::<Vec<_>>();
+    Ok(bytes)
 }
 
-pub fn load_file(filename: &str, size: usize) -> Result<Vec<u8>, Error> {
-    let is = open_input(filename)?;
-    Ok(load_stream(&is, size))
-}
-
-pub fn load_zip_entry(
-    archivename: &str,
-    entryname: &str,
-    _size: usize,
-) -> Result<Vec<u8>, Error> {
+pub fn load_zip_entry(archivename: &str, entryname: &str, _size: usize) -> Result<Vec<u8>, Error> {
     let archive = File::open(archivename)?;
     let mut zip = ZipArchive::new(archive)?;
     let bytes = zip.by_name_raw(entryname)?;
     Ok(bytes)
 }
 
-pub fn open_input(filename: &str) -> Result<File, Error> {
-    Ok(File::open(filename)?)
+// TODO: 返回 Vec<u8> 不是一个明智的选择, 文件太大可能会爆内存
+pub fn open_input_zip_entry(archivename: &str, entryname: &str) -> Result<Vec<u8>, Error> {
+    let archive = File::open(archivename)?;
+    let mut zip = ZipArchive::new(archive)?;
+    let bytes = zip.by_name_raw(entryname)?;
+    Ok(bytes)
+}
+
+pub fn open_input(filename: &str) -> Result<Vec<u8>, Error> {
+    let file = File::open(filename)?;
+    let bytes = file.bytes().map(|b| b.unwrap()).collect::<Vec<_>>();
+    Ok(bytes)
 }
 
 pub fn open_output(filename: &str) -> Result<File, Error> {
